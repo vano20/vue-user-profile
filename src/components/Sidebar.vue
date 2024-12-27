198ffb0fc96f0914420648f7210fb508a8add4de
savano maitama <vano_202228@yahoo.com>
2024-12-27 12:47:58 +0700

    fix: change button color and increase size of floating sidebar button to increase noticeableness

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -65,9 +65,9 @@ onUnmounted(() => {
     </aside>
 
     <div class="absolute left-4 bottom-4 md:hidden z-10">
-      <Button variant="secondary" pill @click="toggle">
+      <Button pill @click="toggle">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-          class="size-4">
+          class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
       </Button>
